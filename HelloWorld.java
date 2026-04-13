39f1bc2d7b59d96c9e009e21acfd4d711ebc27c1
umminenirajasri <ummineni.rajasri@gmail.com>
2026-04-13 09:55:35 -0700

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
     }
 }
